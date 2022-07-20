f9f339bae8c182e5803d366e2dc228c4eab6e7ac
shahryarfareedkhan <shahryarfreed123@gmail.com>
2022-07-20 15:59:29 +0500

    category controller and test

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
   
   # , only: [:show, :index, :new, :create, :edit, :update, :destroy]
 
